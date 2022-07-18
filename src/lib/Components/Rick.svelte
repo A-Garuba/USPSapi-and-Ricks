4426867f5010df03640a2b66ae9f7ce8d04081b6
Alex Garuba <alexandergaruba@gmail.com>
2022-07-18 11:04:04 -0400

    add comments

--- src/lib/Components/Rick.svelte
+++ src/lib/Components/Rick.svelte
@@ -1,83 +1,120 @@
 <script lang="ts">
-    import { fade, fly } from 'svelte/transition';
+	import { fade, fly } from 'svelte/transition';
 
-    import { getRicks } from "$utils/api/rick.util";
+	import { getRicks } from '$utils/api/rick.util';
 
-    let first=0, second=1, third=2;
-    let ricks = [];
-    
-    const fetchRicks = async () => {
-        let response = await getRicks();
-        if (response)
-            ricks = response;
-    }
+	let first = 0,
+		second = 1,
+		third = 2;
+	let ricks = [];
 
-    const shuffle = () => {
-        first += 3; first %= 20;
-        second += 3; second %= 20;
-        third += 3; third %= 20;
-    }
+	const fetchRicks = async () => {
+		let response = await getRicks();
+		if (response) ricks = response;
+	};
 
-    $: console.log(ricks);
-    
+	const shuffle = () => {
+		first += 3;
+		first %= 20;
+		second += 3;
+		second %= 20;
+		third += 3;
+		third %= 20;
+	};
+
+	$: console.log(ricks);
 </script>
 
+<div class="my-10" />
+
+<!-- Rick button makes initial request, then shuffles through response -->
 <div
-    class="section cursor-pointer"
-    in:fade={{delay: 1000}} out:fly={{ x: 100 }}
-    on:click={() => {ricks.length ? shuffle() : fetchRicks()}}
+	class="section cursor-pointer"
+	in:fade={{ delay: 1000 }}
+	out:fly={{ x: 100 }}
+	on:click={() => {
+		ricks.length ? shuffle() : fetchRicks();
+	}}
 >
-    {ricks.length ? 'Shuffle Ricks' : 'Okay cool, now show me ricks'}
+	{ricks.length ? 'Shuffle Ricks' : 'Okay cool, now show me ricks'}
 </div>
 {#if ricks.length}
-    <div class="max-w-xl mx-4 md:mx-auto mb-4 relative">
-        <div class="flex flex-col md:flex-row gap-3">
+	<div class="max-w-xl mx-4 md:mx-auto mb-4 relative">
+		<div class="flex flex-col md:flex-row gap-3">
+			<!-- First Card -->
+			<div class="w-full md:w-1/3 md:h-[325px] rounded-2xl shadow-xl hover:shadow-2xl border p-2">
+				<div class="text-sm text-center truncate">{ricks[first].name}</div>
+				<img
+					class="rounded-xl my-2 mx-auto h-1/2"
+					src={ricks[first].image}
+					alt="some variation of rick sanchez"
+				/>
+				<div class="text-xs text-center md:text-left">
+					First appears in {ricks[first].episode[0].name}
+					{`(${ricks[first].episode[0].episode})`} <br />
+					Has been seen at {ricks[first].location.name} <br />
+					STATUS:
+					<span
+						class={ricks[first].status === 'Alive'
+							? 'text-color-green-primary'
+							: ricks[first].status === 'Dead'
+							? 'text-red-500'
+							: 'text-gray-500'}
+					>
+						{ricks[first].status}
+					</span>
+				</div>
+			</div>
 
-            <div class="w-full md:w-1/3 md:h-[325px] rounded-2xl shadow-xl hover:shadow-2xl border p-2">
-                <div class='text-sm text-center truncate'>{ricks[first].name}</div>
-                <img class='rounded-xl my-2 mx-auto h-1/2' src={ricks[first].image} alt='some variation of rick sanchez' />
-                <div class="text-xs text-center md:text-left">
-                    First appears in {ricks[first].episode[0].name} {`(${ricks[first].episode[0].episode})`} <br />
-                    Has been seen at {ricks[first].location.name} <br />
-                    STATUS: <span class={ricks[first].status === "Alive" ? 'text-color-green-primary'
-                                            : ricks[first].status === "Dead" ? 'text-red-500'
-                                            : 'text-gray-500'}
-                            >
-                                {ricks[first].status}
-                            </span>
-                </div>
-            </div>
+			<!-- Second Card -->
+			<div class="w-full md:w-1/3 md:h-[325px] rounded-2xl shadow-xl hover:shadow-2xl border p-2">
+				<div class="text-sm text-center truncate">{ricks[second].name}</div>
+				<img
+					class="rounded-xl my-2 mx-auto h-1/2"
+					src={ricks[second].image}
+					alt="some variation of rick sanchez"
+				/>
+				<div class="text-xs text-center md:text-left">
+					First appears in {ricks[second].episode[0].name}
+					{`(${ricks[second].episode[0].episode})`} <br />
+					Has been seen at {ricks[second].location.name} <br />
+					STATUS:
+					<span
+						class={ricks[second].status === 'Alive'
+							? 'text-color-green-primary'
+							: ricks[second].status === 'Dead'
+							? 'text-red-500'
+							: 'text-gray-500'}
+					>
+						{ricks[second].status}
+					</span>
+				</div>
+			</div>
 
-            <div class="w-full md:w-1/3 md:h-[325px] rounded-2xl shadow-xl hover:shadow-2xl border p-2">
-                <div class='text-sm text-center truncate'>{ricks[second].name}</div>
-                <img class='rounded-xl my-2 mx-auto h-1/2' src={ricks[second].image} alt='some variation of rick sanchez' />
-                <div class="text-xs text-center md:text-left">
-                    First appears in {ricks[second].episode[0].name} {`(${ricks[second].episode[0].episode})`} <br />
-                    Has been seen at {ricks[second].location.name} <br />
-                    STATUS: <span class={ricks[second].status === "Alive" ? 'text-color-green-primary'
-                                            : ricks[second].status === "Dead" ? 'text-red-500'
-                                            : 'text-gray-500'}
-                            >
-                                {ricks[second].status}
-                            </span>
-                </div>
-            </div>
-
-            <div class="w-full md:w-1/3 md:h-[325px] rounded-2xl shadow-xl hover:shadow-2xl border p-2">
-                <div class='text-sm text-center truncate'>{ricks[third].name}</div>
-                <img class='rounded-xl my-2 mx-auto h-1/2' src={ricks[third].image} alt='some variation of rick sanchez' />
-                <div class="text-xs text-center md:text-left">
-                    First appears in {ricks[third].episode[0].name} {`(${ricks[third].episode[0].episode})`} <br />
-                    Has been seen at {ricks[third].location.name} <br />
-                    STATUS: <span class={ricks[third].status === "Alive" ? 'text-color-green-primary'
-                                            : ricks[third].status === "Dead" ? 'text-red-500'
-                                            : 'text-gray-500'}
-                            >
-                                {ricks[third].status}
-                            </span>
-                </div>
-            </div>
-
-        </div>
-    </div>
+			<!-- Third Card -->
+			<div class="w-full md:w-1/3 md:h-[325px] rounded-2xl shadow-xl hover:shadow-2xl border p-2">
+				<div class="text-sm text-center truncate">{ricks[third].name}</div>
+				<img
+					class="rounded-xl my-2 mx-auto h-1/2"
+					src={ricks[third].image}
+					alt="some variation of rick sanchez"
+				/>
+				<div class="text-xs text-center md:text-left">
+					First appears in {ricks[third].episode[0].name}
+					{`(${ricks[third].episode[0].episode})`} <br />
+					Has been seen at {ricks[third].location.name} <br />
+					STATUS:
+					<span
+						class={ricks[third].status === 'Alive'
+							? 'text-color-green-primary'
+							: ricks[third].status === 'Dead'
+							? 'text-red-500'
+							: 'text-gray-500'}
+					>
+						{ricks[third].status}
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
 {/if}
--- src/lib/Pages/Home.svelte
+++ src/lib/Pages/Home.svelte
@@ -1,136 +1,146 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    import { XMLParser } from 'fast-xml-parser';
+	import { goto } from '$app/navigation';
+	import { XMLParser } from 'fast-xml-parser';
 
-    import { verifyPlace } from "$utils/api/address.util";
+	import { verifyPlace } from '$utils/api/address.util';
 
-    import Address from "$components/Address.svelte";
-    import Ricks from "$components/Rick.svelte";
+	import Address from '$components/Address.svelte';
+	import Ricks from '$components/Rick.svelte';
 
-    const parser = new XMLParser();
+	const parser = new XMLParser();
 
-    let values: Place = {
-        address: '',
-        suiteOrApt: '',
-        city: '',
-        state: '',
-        zip: ''
-    }
+	let values: Place = {
+		address: '',
+		suiteOrApt: '',
+		city: '',
+		state: '',
+		zip: ''
+	};
 
-    let USPSAddress;
-    let displayAddress: Place;
+	let USPSAddress;
+	let displayAddress: Place;
 
-    const updateUSPS = async () => {
-        if (values.address.length > 0 || values.suiteOrApt.length > 0 || values.city.length > 0 ||values.state.length > 0 || values.zip.length > 0) {
-            let response = await verifyPlace(values);
-            let parsedRes = parser.parse(response);
+	const updateUSPS = async () => {
+		if (
+			values.address.length > 0 ||
+			values.suiteOrApt.length > 0 ||
+			values.city.length > 0 ||
+			values.state.length > 0 ||
+			values.zip.length > 0
+		) {
+			let response = await verifyPlace(values);
+			let parsedRes = parser.parse(response);
 
-            // if we got an address back, update UI
-            if (!parsedRes.AddressValidateResponse.Address.Error) {
-                USPSAddress = parsedRes.AddressValidateResponse.Address;
-                displayAddress = {
-                    address: USPSAddress.Address2 || '',
-                    suiteOrApt: USPSAddress.Address1 || '',
-                    city: USPSAddress.City || '',
-                    state: USPSAddress.State || '',
-                    zip: `${USPSAddress.Zip5}-${USPSAddress.Zip4}` || ''
-                }
-            } else {
-                displayAddress = null;
-            }
-        }
-    }
+			// if we got an address back, update UI
+			if (!parsedRes.AddressValidateResponse.Address.Error) {
+				USPSAddress = parsedRes.AddressValidateResponse.Address;
+				displayAddress = {
+					address: USPSAddress.Address2 || '',
+					suiteOrApt: USPSAddress.Address1 || '',
+					city: USPSAddress.City || '',
+					state: USPSAddress.State || '',
+					zip: `${USPSAddress.Zip5}-${USPSAddress.Zip4}` || ''
+				};
+			} else {
+				displayAddress = null;
+			}
+		}
+	};
 
-    //$: console.log(USPSAddress);
+	//$: console.log(USPSAddress);
 </script>
 
 <div class="section">
+	<div class="absolute top-0 left-0 p-4">
+		<img
+			class="w-8 h-8 rounded object-cover cursor-pointer hover:scale-110 hover:drop-shadow-xl"
+			src="static/images/favicon.png"
+			alt="GoodR Co Logo"
+			on:click={() => goto('https://goodr.co/')}
+		/>
+	</div>
 
-    <div class= "absolute top-0 left-0 p-4">
-        <img
-            class="w-8 h-8 rounded object-cover cursor-pointer hover:scale-110 hover:drop-shadow-xl"
-            src="static/images/favicon.png"
-            alt="GoodR Co Logo"
-            on:click={() => goto('https://goodr.co/')}
-        />
-    </div>
+	<div class="text-lg md:text-2xl px-16 md:px-0 text-center">
+		Enter an Address for your Pickup Request
+	</div>
 
-    <div class="text-lg md:text-2xl px-16 md:px-0 text-center">Enter an Address for your Pickup Request</div>
+	<!-- Address form -->
+	<div class="flex flex-col gap-3 mt-4">
+		<input
+			class="w-full {values.address ? 'border-color-green-primary' : 'border-gray-300'}"
+			placeholder="Street"
+			on:input={(e) => {
+				values.address = e.currentTarget.value;
+				updateUSPS();
+			}}
+		/>
 
-    <!-- Address form -->
-    <div class="flex flex-col gap-3 mt-4">
-        
-        <input class="w-full {values.address ? 'border-color-green-primary' : 'border-gray-300'}"
-            placeholder="Street"
-            on:input={(e) => {
-                values.address = e.currentTarget.value;
-                updateUSPS();
-            }}
-        >
+		<input
+			class="w-full {values.suiteOrApt ? 'border-color-green-primary' : 'border-gray-300'}"
+			placeholder="Apt/Suite #"
+			on:input={(e) => {
+				values.suiteOrApt = e.currentTarget.value;
+				updateUSPS();
+			}}
+		/>
 
-        <input class="w-full {values.suiteOrApt ? 'border-color-green-primary' : 'border-gray-300'}"
-            placeholder="Apt/Suite #"
-            on:input={(e) => {
-                values.suiteOrApt = e.currentTarget.value;
-                updateUSPS();
-            }}
-        >
-    
-        <div class="flex flex-row gap-3">
-            <input class="grow {values.city ? 'border-color-green-primary' : 'border-gray-300'}"
-                placeholder="City"
-                on:input={(e) => {
-                    values.city = e.currentTarget.value;
-                    updateUSPS();
-                }}
-            >
-            
-            <input class="w-1/5 {values.state ? 'border-color-green-primary' : 'border-gray-300'}"
-                placeholder="State"
-                on:input={(e) => {
-                    values.state = e.currentTarget.value;
-                   updateUSPS();
-                }}
-            >
-        
-            <input class="w-1/5 {values.zip ? 'border-color-green-primary' : 'border-gray-300'}"
-                placeholder="Zip"
-                on:input={(e) => {
-                    values.zip = e.currentTarget.value;
-                    updateUSPS();
-                }}
-            >
-        </div>
-    
-        <!-- <div class="flex flex-row gap-3">
+		<div class="flex flex-row gap-3">
+			<input
+				class="grow {values.city ? 'border-color-green-primary' : 'border-gray-300'}"
+				placeholder="City"
+				on:input={(e) => {
+					values.city = e.currentTarget.value;
+					updateUSPS();
+				}}
+			/>
+
+			<input
+				class="w-1/5 {values.state ? 'border-color-green-primary' : 'border-gray-300'}"
+				placeholder="State"
+				on:input={(e) => {
+					values.state = e.currentTarget.value;
+					updateUSPS();
+				}}
+			/>
+
+			<input
+				class="w-1/5 {values.zip ? 'border-color-green-primary' : 'border-gray-300'}"
+				placeholder="Zip"
+				on:input={(e) => {
+					values.zip = e.currentTarget.value;
+					updateUSPS();
+				}}
+			/>
+		</div>
+
+		<!-- <div class="flex flex-row gap-3">
             <input class="grow"
                     placeholder="County">
 
             <input class="grow"
                 placeholder="Country">
         </div> -->
-        
-    </div>
+	</div>
 </div>
 
 {#if displayAddress}
-    <Address
-        bind:address={displayAddress.address}
-        suiteOrApt={displayAddress.suiteOrApt}
-        city={displayAddress.city}
-        state={displayAddress.state}
-        zip={displayAddress.zip}
-    />
-        
-    <Ricks />
+	<!-- Formatted API response -->
+	<Address
+		bind:address={displayAddress.address}
+		suiteOrApt={displayAddress.suiteOrApt}
+		city={displayAddress.city}
+		state={displayAddress.state}
+		zip={displayAddress.zip}
+	/>
+
+	<Ricks />
 {/if}
 
-
 <style lang="postcss">
-    input {
-        @apply rounded p-1;
-        @apply border-2;
-        @apply focus:border-color-green-primary focus:shadow-lg;
-        @apply hover:border-color-green-primary hover:shadow-lg;
-    }
+	input {
+		@apply rounded p-1;
+		@apply border-2;
+		@apply focus:border-color-green-primary focus:shadow-lg;
+		@apply hover:border-color-green-primary hover:shadow-lg;
+	}
 </style>
